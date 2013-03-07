57ebfebbb28dc53eb7ad826aba34d139237cf45d
Kevin Hankens <email@kevinhankens.com>
2013-03-06 21:53:35 -0500

    Faster than fgrep\!

--- ext/statsr/statsr.c
+++ ext/statsr/statsr.c
@@ -3,34 +3,46 @@
 #include <string.h>
 #include <stdlib.h>
 
-static VALUE query(VALUE self, VALUE query_ns, int query_limit) {
+static VALUE query(VALUE self, VALUE logfile, VALUE query_ns, int query_limit) {
+  // File pointer.
   FILE * file;
-  //char line[256];
+  // File line max length.
   int line_size = 256;
   char *line = (char *) malloc(line_size);
+  const char *filepath = RSTRING_PTR(logfile);
+  const char *query_ns_char = RSTRING_PTR(query_ns);
 
-  VALUE statsr_key_ts = rb_str_new2("ts");
-  VALUE statsr_key_ns = rb_str_new2("ns");
-  VALUE statsr_key_v = rb_str_new2("v");
+  // Create symbols by passing ruby strings into rb_str_intern.
+  VALUE statsr_key_ts = rb_str_intern(rb_str_new2("ts"));
+  VALUE statsr_key_ns = rb_str_intern(rb_str_new2("ns"));
+  VALUE statsr_key_v = rb_str_intern(rb_str_new2("v"));
+  // Create an empty string for comparison.
   VALUE statsr_str_empty = rb_str_new2("");
 
-  int statsr_timestamp;
-  int statsr_value;
+  // Convert into an int that ruby understands.
   int limit = NUM2INT(query_limit);
 
+  // Return array instantiation.
   VALUE statsr_data = rb_ary_new();
 
-  file = fopen ( "/var/log/statstest.log" , "rb" );
+  file = fopen (filepath, "rb");
   if (file==NULL) {fputs ("File error",stderr); exit (1);}
 
   int count = 0;
 
   while (NULL != fgets(line, line_size, file) && count < limit) {
-    if (line[0] != '\0' && line[0] != '\n') {
+
+    size_t len = strlen(line) - 1;
+ 
+    if (line[len] == '\n');
+        line[len] = '\0';
+
+    if (line[0] != '\0' && line[0] != '\n' && strstr(line, query_ns_char)) {
+    //if (line[0] != '\0' && line[0] != '\n') {
       VALUE statsr_event = rb_hash_new();
       VALUE statsr_str_ts = rb_str_new2(strtok(line, "\t"));
       VALUE statsr_str_ns = rb_str_new2(strtok(NULL, "\t"));
-      VALUE statsr_str_v = rb_str_new2(strtok(NULL, "\n"));
+      VALUE statsr_str_v = rb_str_new2(strtok(NULL, "\0"));
 
       if (rb_str_cmp(query_ns, statsr_str_empty) == 0 || rb_str_cmp(query_ns, statsr_str_ns) == 0) {
         rb_hash_aset(statsr_event, statsr_key_ts, statsr_str_ts);
@@ -56,5 +68,5 @@ void Init_statsr(void) {
       rb_cObject);
 
   // Sets up the data
-  rb_define_singleton_method(klass, "query", query, 2);
+  rb_define_singleton_method(klass, "query", query, 3);
 }
--- test.rb
+++ test.rb
@@ -1,10 +1,44 @@
 #!/usr/bin/env ruby
 
+time_start = Time.now.to_f
+
 require 'statsr'
 require 'pp'
 
-s = Statsr.query ARGV[0], ARGV[1].to_i
+time_require = Time.now.to_f
+
+puts "GEM"
+
+s = Statsr.query "/var/log/statstest.log", ARGV[0], ARGV[1].to_f
 puts s.length
 pp s[0]
 pp s[1]
 pp s[2]
+pp s[s.length - 3]
+pp s[s.length - 2]
+pp s[s.length - 1]
+
+time_gem = Time.now.to_f
+
+puts "TICKS"
+
+ss = []
+`fgrep #{ARGV[0]} /var/log/statstest.log | head -n#{ARGV[1]}`.split("\n").each do |line|
+  l = line.split("\t")
+  ss.push({:ts => l[0], :ns => l[1], :v => l[2]})  
+end
+
+puts ss.length
+pp ss[0]
+pp ss[1]
+pp ss[2]
+pp ss[ss.length - 3]
+pp ss[ss.length - 2]
+pp ss[ss.length - 1]
+
+time_tick = Time.now.to_f
+
+puts "Require: #{time_require - time_start}"
+puts "Query: #{time_gem - time_require}"
+puts "Ticks: #{time_tick - time_gem}"
+puts "Total: #{time_tick - time_start}"
