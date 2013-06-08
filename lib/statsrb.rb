b6a2d0ae4e68a7c7a327b5651f691cf1e187b76f
Kevin Hankens <email@kevinhankens.com>
2013-06-08 12:03:18 -0400

    Moving docs into main statsr class file.

--- lib/statsrb.rb
+++ lib/statsrb.rb
@@ -1,4 +1,37 @@
 require 'statsrb/statsrb'
 
+# @author Kevin Hankens
 class Statsrb
+  # [!{:ts => Time.now.to_i, :ns => "test", :v => 33}]
+  attr_accessor :data
+
+  # Writes the @data in memory to a specified file.
+  # @param filepath [String]
+  # @param filemode [String]
+  # @return Statsrb
+  def write filepath, filemode
+  end
+
+  # Splits namespaces in @data in memory to a separate files.
+  # @param filepath [String]
+  # @param filemode [String]
+  # @return Statsrb
+  def split_write filepath, filemode
+  end
+
+  # Locates data from a specified file and loads into @data.
+  # @param filepath [String]
+  # @param namespace [String]
+  # @param limit [Number]
+  # @param start_time [Number]
+  # @param end_time [Number]
+  # @return Statsrb
+  def query filepath, namespace, limit, start_time, end_time
+  end
+
+  # Returns a rack-compatable response.
+  # @param env [Hash]
+  def sort env
+  end
+
 end
